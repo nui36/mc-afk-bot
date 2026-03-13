c5b810bf95addbcdacd0673c8ffe869de0bb9c55
nui36 <137643770+nui36@users.noreply.github.com>
2026-03-14 00:23:57 +0700

    Use environment variable for Discord token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 // ================= [ ตั้งค่าข้อมูลของคุณตรงนี้ ] =================
-const DISCORD_TOKEN = 'MTQ4MjA1ODIzMTY0Mjg0OTQwMw.GYRmMT.M42rlCalW5LRpkSE2se0VHXPFcL3gyCfQiqtn0';
+process.env.DISCORD_TOKEN;
 const TEXT_CH_ID = '1482064161038139403'; // ห้องที่ให้บอทพิมพ์แจ้งคนเข้า-ออก
 const STATUS_VC_ID = '1482064161038139403'; // เช่น 🟢 Server: Online
 const PLAYER_VC_ID = '1482064304118304810'; // เช่น 👥 Players: 1/20
